710a2142e53bc52fc51f63a5f20e4630c1fbfc44
Roland Stevenson <roland@rmg-services.com>
2019-03-11 14:53:35 +0000

    added optional double quotes to regexp

--- branch_and_build.sh
+++ branch_and_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LINUX_FLAVOR=$(cat /etc/os-release | sed -n '/^ID=/ s/^ID="\(.*\)"/\1/p')
-VERSION_ID=$(cat /etc/os-release | sed -n '/^VERSION_ID/ s/^VERSION_ID="\(.*\)"/\1/p')
+LINUX_FLAVOR=$(cat /etc/os-release | sed -n '/^ID=/ s/^ID="*\(.*\)"*/\1/p')
+VERSION_ID=$(cat /etc/os-release | sed -n '/^VERSION_ID/ s/^VERSION_ID="*\(.*\)"*/\1/p')
 R_VERSION=$(R --version | sed -n '/R version/ s/.* version \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/p')
 
 BRANCH_NAME=dev-linux-${LINUX_FLAVOR}${VERSION_ID}-R$R_VERSION
